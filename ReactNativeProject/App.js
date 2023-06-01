00092cce11af9bc1020976b89a3fae11e7e908cf
Vitalii Koshytskyi <115081482+VitaliiKoshytskyi@users.noreply.github.com>
2023-06-01 14:42:27 +0300

    hw2

--- ReactNativeProject/App.js
+++ ReactNativeProject/App.js
@@ -1,13 +1,32 @@
+import {ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { useFonts } from "expo-font";
+import bkgImage from "./assets/images/Photo.png"
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import LoginScreen from './Screens/LoginScreen';
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your appHEY!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+  const [fontLoaded] = useFonts({
+    "Roboto-regular": require("./assets/fonts/Roboto/Roboto-Regular.ttf"),
+    "Roboto-medium": require("./assets/fonts/Roboto/Roboto-Medium.ttf"),
+    "Roboto-bolt": require("./assets/fonts/Roboto/Roboto-Bold.ttf"),
+  });
+
+  if (!fontLoaded) {
+    return null;
+  }
+   return (
+     <>
+       <ImageBackground source={bkgImage} style={styles.image}>
+          {/* <RegistrationScreen /> */}
+        <LoginScreen />
+       </ImageBackground>
+     </>
+      
+      
+  )
 }
 
 const styles = StyleSheet.create({
@@ -16,5 +35,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  
   },
+  image: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent:'center'
+  }
 });
---
+++ ReactNativeProject/Screens/LoginScreen.jsx
@@ -0,0 +1,97 @@
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+} from "react-native";
+
+export default function LoginScreen() {
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <View style={styles.box}>
+        <Text style={styles.title}>Увійти</Text>
+        <View>
+          <TextInput
+            style={styles.input}
+            placeholder="Адреса електронної пошти"
+          />
+          <TextInput style={styles.input} placeholder="Пароль" />
+          <TouchableOpacity style={styles.hideBtn}>
+            <Text style={styles.hideText}>Показати</Text>
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.btnText}>Увійти</Text>
+        </TouchableOpacity>
+        <Text style={styles.link}>Немає акаунту? Зареєструватися</Text>
+      </View>
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  box: {
+    width: "100%",
+    backgroundColor: "#fff",
+    paddingTop: 32,
+    paddingHorizontal: 16,
+    paddingBottom: 144,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+  },
+  title: {
+    fontFamily: "Roboto-medium",
+    fontSize: 30,
+    marginBottom: 32,
+    textAlign: "center",
+  },
+  input: {
+    minWidth: 343,
+    height: 50,
+    backgroundColor: "#F6F6F6",
+    borderColor: "#E8E8E8",
+    borderStyle: "solid",
+    borderWidth: 1,
+    borderRadius: 8,
+    padding: 16,
+    marginBottom: 16,
+    fontFamily: "Roboto-regular",
+    fontSize: 16,
+  },
+  hideBtn: {
+    position: "absolute",
+    right: 16,
+    bottom: 31,
+  },
+  hideText: {
+    color: "#1B4371",
+    fontFamily: "Roboto-regular",
+    fontSize: 16,
+  },
+  btn: {
+    minWidth: "100%",
+    height: 51,
+    marginTop: 27,
+    paddingVertical: 16,
+    paddingHorizontal: 32,
+    borderRadius: 100,
+    backgroundColor: "#FF6C00",
+  },
+  btnText: {
+    color: "#fff",
+    textAlign: "center",
+    fontFamily: "Roboto-regular",
+    fontSize: 16,
+  },
+  link: {
+    marginTop: 16,
+    color: "#1B4371",
+    textAlign: "center",
+    fontFamily: "Roboto-regular",
+    fontSize: 16,
+  },
+});
