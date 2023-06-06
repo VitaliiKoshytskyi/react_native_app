e79b920137c0796614d6ae625c34950338ff050c
Vitalii Koshytskyi <115081482+VitaliiKoshytskyi@users.noreply.github.com>
2023-06-06 12:53:02 +0300

    updated HomeScreen

--- ReactNativeProject/App.js
+++ ReactNativeProject/App.js
@@ -1,14 +1,35 @@
 import 'react-native-gesture-handler';
 import { StyleSheet, Text, View } from 'react-native';
 import { useFonts } from "expo-font";
+import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
 import LoginScreen from './Screens/Auth/LoginScreen';
 import RegistrationScreen from './Screens/Auth/RegistrationScreen';
-import { NavigationContainer } from '@react-navigation/native';
+// import PostsScreen from './Screens/Main/PostsScreen';
+// import CreatePostsScreen from './Screens/Main/CreatePostsScreen';
+// import ProfileScreen from './Screens/Main/ProfileScreen';
+import HomeScreen from './Screens/Main/Home';
 
-const AuthStack = createStackNavigator()
+const AuthStack = createStackNavigator();
+const MainTab = createBottomTabNavigator();
+
+const useRoute = (isAuth) => {
+  if (!isAuth) {
+    return <AuthStack.Navigator initialRouteName="Login">
+           <AuthStack.Screen options={{headerShown:false}} name="Registration" component={RegistrationScreen} />
+           <AuthStack.Screen  options={{headerShown:false}} name="Login" component={LoginScreen} />
+       </AuthStack.Navigator> 
+  }
+  return  <MainTab.Navigator>
+         <MainTab.Screen name="Home" component={HomeScreen} options={{headerShown:false}}/>
+       </MainTab.Navigator>
+}
 
 export default function App() {
+
+const routing = useRoute({})
 
   const [fontLoaded] = useFonts({
     "Roboto-regular": require("./assets/fonts/Roboto/Roboto-Regular.ttf"),
@@ -21,10 +42,7 @@ export default function App() {
   }
    return (
      <NavigationContainer>
-       <AuthStack.Navigator initialRouteName="Login">
-           <AuthStack.Screen options={{headerShown:false}} name="Registration" component={RegistrationScreen} />
-           <AuthStack.Screen  options={{headerShown:false}} name="Login" component={LoginScreen} />
-       </AuthStack.Navigator>
+      {routing}
      </NavigationContainer>
 
   )
--- ReactNativeProject/Screens/Main/Home.jsx
+++ ReactNativeProject/Screens/Main/Home.jsx
@@ -1,10 +1,74 @@
-import { View, Text } from "react-native";
+import { View, Text,StyleSheet,TouchableOpacity  } from "react-native";
+import { Ionicons, Feather, AntDesign } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { useNavigation } from '@react-navigation/native';
+
+import PostsScreen from "./PostsScreen";
+import CreatePostsScreen from "./CreatePostsScreen";
+import ProfileScreen from "./ProfileScreen";
+
+const Tabs = createBottomTabNavigator();
 
 export default function HomeScreen() {
+   const navigation = useNavigation();
+ 
+    
   return (
-    <View style={styles.container}>
-      <Text>HomeScreen</Text>
-    </View>
+    <Tabs.Navigator
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          alignItems: "center",
+          paddingHorizontal: 60,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="PostsScreen"
+        component={PostsScreen}
+        options={{
+          title: "Публікації",
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons name="grid-outline" size={size} color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="CreatePostsScreen"
+        component={CreatePostsScreen}
+        options={{
+          title: "Створити публікацію",
+          tabBarButton: () => (
+            <TouchableOpacity
+              style={{
+                alignSelf: "center",
+                width: 70,
+                height: 40,
+                backgroundColor: "#FF6C00",
+                borderRadius: 20,
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+              onPress={() => navigation.navigate("CreatePostsScreen")}
+            >
+              <AntDesign name="plus" size={15} color="white" />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="ProfileScreen"
+        component={ProfileScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ focused, color, size }) => (
+            <Feather name="user" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs.Navigator>
+  
   );
 }
 
